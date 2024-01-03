3846095cd81d0240071cf2f3d25982da73c0ac8f
yroslav-js <75677226+yroslav-js@users.noreply.github.com>
2024-01-03 15:31:47 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -36,24 +36,70 @@ function closeSidebar() {
 	body.classList.remove('open')
 }
 
-// const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
+const menuLinks = document.querySelectorAll('.menu__item[data-goto]')
 
-// if (menuLinks.length > 0) {
-// 	menuLinks.forEach(link => {
-// 		link.addEventListener("click", onMenuLinkClick)
-// 	})
-// 	function onMenuLinkClick(e) {
-// 		closeSidebar()
-// 		const menuLink = e.target
-// 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-// 			const gotoBlock = document.querySelector(menuLink.dataset.goto)
-// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset
+if (menuLinks.length > 0) {
+	menuLinks.forEach(link => {
+		link.addEventListener("click", onMenuLinkClick)
+	})
+	function onMenuLinkClick(e) {
+		closeSidebar()
+		const menuLink = e.target
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto)
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset
 
-// 			window.scrollTo({
-// 				top: gotoBlockValue,
-// 				behavior: "smooth"
-// 			})
-// 			e.preventDefault()
-// 		}
-// 	}
-// }
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			})
+			e.preventDefault()
+		}
+	}
+}
+
+const plan = document.getElementById('plan');
+const one = document.getElementById('case-one');
+const two = document.getElementById('case-two');
+
+const func = (box) => {
+	let isDown = false;
+	let startX;
+	let startY;
+	let scrollLeft;
+	let scrollTop;
+
+	box.addEventListener('mousedown', (e) => {
+		isDown = true;
+		startX = e.pageX - box.offsetLeft;
+		startY = e.pageY - box.offsetTop;
+		scrollLeft = box.scrollLeft;
+		scrollTop = box.scrollTop;
+		box.style.cursor = 'grabbing';
+	});
+
+	box.addEventListener('mouseleave', () => {
+		isDown = false;
+		box.style.cursor = 'grab';
+	});
+
+	box.addEventListener('mouseup', () => {
+		isDown = false;
+		box.style.cursor = 'grab';
+	});
+
+	document.addEventListener('mousemove', (e) => {
+		if (!isDown) return;
+		e.preventDefault();
+		const x = e.pageX - box.offsetLeft;
+		const y = e.pageY - box.offsetTop;
+		const walkX = (x - startX) * 1; // Change this number to adjust the scroll speed
+		const walkY = (y - startY) * 1; // Change this number to adjust the scroll speed
+		box.scrollLeft = scrollLeft - walkX;
+		box.scrollTop = scrollTop - walkY;
+	});
+}
+
+func(plan)
+func(one)
+func(two)
